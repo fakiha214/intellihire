99c20334dbdfa4273c784fbbbbf862509ecb771e
fakiha214 <fakiha214@gmail.com>
2025-12-31 13:00:00 -0500

    Updated requirements.txt

--- src/routes/Settings.jsx
+++ src/routes/Settings.jsx
@@ -542,3 +542,13 @@ const handleFormSubmit = (e, formData, callback) => {
   }
   callback(formData);
 };
+
+// Feature: State management
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_LOADING': return { ...state, loading: action.payload };
+    case 'SET_ERROR': return { ...state, error: action.payload };
+    case 'SET_DATA': return { ...state, data: action.payload };
+    default: return state;
+  }
+};
